3b6d00f196d418d8b39d028d4d6f73adc1aaaf60
Johnny Rico <rubydev@qq.com>
2016-01-26 00:27:22 +1000

    add tests

--- spec/features/cancan_spec.rb
+++ spec/features/cancan_spec.rb
@@ -7,13 +7,13 @@ describe "User" do
   let(:user){ create(:user) }
   let(:ability) { AdminAbility.new(user) }
 
-    it "when user is admin he can manage all" do
-      user.add_role :admin
-      ability.should be_able_to(:manage, :all)
-    end
+  it "when user is admin he can manage all" do
+    user.add_role :admin
+    ability.should be_able_to(:manage, :all)
+  end
 
-    it "when user is user he couldn't manage all" do
-      user.add_role :user
-      ability.should_not be_able_to(:manage, :all)
-    end
+  it "when user is user he couldn't manage all" do
+    user.add_role :user
+    ability.should_not be_able_to(:manage, :all)
+  end
 end
